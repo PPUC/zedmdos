ec601a674345cba7690e15309bfe864cc7c27390
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2025-06-16 20:49:05 +0000

    update br + fixes
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/zedmdos/libppuc/libppuc.mk
+++ package/zedmdos/libppuc/libppuc.mk
@@ -9,10 +9,19 @@ LIBPPUC_SITE = $(call github,PPUC,libppuc,$(LIBPPUC_VERSION))
 LIBPPUC_LICENSE = GPLv3
 LIBPPUC_LICENSE_FILES = LICENSE
 
-LIBDMDUTIL_DEPENDENCIES = io-boards libserialport libyaml-cpp
+LIBPPUC_DEPENDENCIES = io-boards libserialport libyaml
 
 LIBPPUC_CONF_OPTS += -DPLATFORM=linux
 LIBPPUC_CONF_OPTS += -DBUILD_STATIC=off
+
+define LIBPPUC_POST_INSTALL_STAGING_INCLUDES
+  mkdir -p $(STAGING_DIR)/usr/include/libppuc
+  cp $(@D)/src/PPUC_structs.h $(STAGING_DIR)/usr/include/
+endef
+
+LIBPPUC_POST_INSTALL_STAGING_HOOKS += LIBPPUC_POST_INSTALL_STAGING_INCLUDES
+
+
 LIBPPUC_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
