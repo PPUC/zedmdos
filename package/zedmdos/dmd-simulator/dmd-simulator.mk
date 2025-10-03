4dd9f5027cd2587404a8589c33fc14d83c9566ed
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2025-10-03 15:57:18 +0000

    package dmd-play-rust, and defaults dmd-play to the rust version
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/zedmdos/dmd-simulator/dmd-simulator.mk
+++ package/zedmdos/dmd-simulator/dmd-simulator.mk
@@ -3,9 +3,17 @@
 # DMD_SIMULATOR
 #
 ################################################################################
-DMD_SIMULATOR_VERSION = 779c90a83dc9a68fded6dd4757bd7282b6d0b624
+DMD_SIMULATOR_VERSION = 6298b07d0a083f4531236adb54d4d272e82f297b
 DMD_SIMULATOR_SITE =  $(call github,batocera-linux,dmd-simulator,$(DMD_SIMULATOR_VERSION))
 DMD_SIMULATOR_SETUP_TYPE = pep517
 DMD_SIMULATOR_DEPENDENCIES = host-python-hatchling
+
+define DMD_SIMULATOR_DMD_PLAY_SYMLINK
+	ln -sf dmd-play-python $(TARGET_DIR)/usr/bin/dmd-play
+endef
+
+ifeq ($(BR2_PACKAGE_DMD_SIMULATOR_DMD_PLAY_BIN),y)
+DMD_SIMULATOR_POST_INSTALL_TARGET_HOOKS += DMD_SIMULATOR_DMD_PLAY_SYMLINK
+endif
 
 $(eval $(python-package))
